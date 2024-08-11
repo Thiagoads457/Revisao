d887316287b4288807b0bf54afbf7da9419da224
Thiagoads457 <thiagoads457@gmail.com>
2024-08-11 19:09:45 -0300

    19:09

---
+++ exer5resp.php
@@ -0,0 +1,11 @@
+<?php
+
+    require_once"cabecalho.php";
+
+    $dias_trabalhados = $_POST['dias_trabalhados'];
+    
+
+    echo "<p>Ferias totais: ". $dias_ferias."</p>";
+
+
+    require_once "rodape.php";
