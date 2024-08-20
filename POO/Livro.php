827b86ec5958f4dc3bd5f2e2013492e21813c2d3
Thiagoads457 <162389672+Thiagoads457@users.noreply.github.com>
2024-08-19 21:26:16 -0300

    Update Livro.php

--- POO/Livro.php
+++ POO/Livro.php
@@ -5,6 +5,12 @@ class Livro {
     private $autor;
     private $ano;
 
+    public function __construct($titulo, $autor, $ano){
+        $this->titulo = $titulo;
+        $this->autor = $autor;
+        $this->ano = $ano;
+    }
 }
 
 $obj = new Livro();
+
